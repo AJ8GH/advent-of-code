bae73c8b9e9d148407e6c0e56f31fc6081d29aa5
Adam Jonas <adam.jonas@ppb.com>
2021-12-04 15:36:06 +0000

    Refactor and add day 3 input

--- src/main/java/day2/DistanceTracker.java
+++ src/main/java/day2/DistanceTracker.java
@@ -1,67 +1,35 @@
 package day2;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DistanceTracker {
-    private static final String INPUT = "./src/main/java/day2/input.txt";
     private static final String FORWARD = "forward";
     private static final String DOWN = "down";
     private static final String UP = "up";
     private static final String DELIMITER = " ";
 
-    private final Map<String, Integer> stepsMap = new HashMap<>();
-    private final List<String> stepsList = new ArrayList<>();
-
     public static void main(String[] args) {
         var tracker = new DistanceTracker();
+        var deserializer = new Deserializer();
 
         // part 1
-        tracker.deserializeInputPart1();
-        System.out.println(tracker.stepsMap);
-        System.out.println(tracker.getDistanceByDepth());
+        var stepsMap = deserializer.deserializeInputPart1();
+        int solution1 = tracker.getDistanceByDepth(stepsMap);
+        System.out.println(solution1);
 
         // part 2
-        tracker.deserializeInputPart2();
-        System.out.println(tracker.getDistanceByDepthWithAim());
+        var stepsList = deserializer.deserializeInputPart2();
+        int solution2 = tracker.getDistanceByDepthWithAim(stepsList);
+        System.out.println(solution2);
     }
 
-    private void deserializeInputPart1() {
-        try (var reader = new BufferedReader(new FileReader(INPUT))) {
-            String step;
-            while ((step = reader.readLine()) != null) {
-                String[] stepSplit = step.trim().split(DELIMITER);
-                String direction = stepSplit[0];
-                int distance = Integer.parseInt(stepSplit[1]);
-                stepsMap.put(direction, stepsMap.getOrDefault(direction, 0) + distance);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private int getDistanceByDepth() {
+    private int getDistanceByDepth(Map<String, Integer> stepsMap) {
         int depth = stepsMap.get(DOWN) - stepsMap.get(UP);
         return depth * stepsMap.get(FORWARD);
     }
 
-    private void deserializeInputPart2() {
-        try (var reader = new BufferedReader(new FileReader(INPUT))) {
-            String step;
-            while((step = reader.readLine()) != null) {
-                stepsList.add(step.trim());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private int getDistanceByDepthWithAim() {
+    private int getDistanceByDepthWithAim(List<String> stepsList) {
         int aim = 0;
         int horizontalDistance = 0;
         int depth = 0;
