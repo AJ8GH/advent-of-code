4d04c1f70cc946ea2addecec11ba1a3992c28971
Adam Jonas <adamjonas25@gmail.com>
2022-11-06 22:30:58 +0000

    Add create script

---
+++ create.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+mkdir ${PWD}/y-2015/src/main/java/io/github/aj8gh/aoc/y15/day${1}
+mkdir ${PWD}/y-2015/src/main/resources/day${1}
+touch ${PWD}/y-2015/src/main/resources/day${1}/README.md
+touch ${PWD}/y-2015/src/main/resources/day${1}/input.txt
+touch ${PWD}/y-2015/src/main/resources/day${1}/example.txt
