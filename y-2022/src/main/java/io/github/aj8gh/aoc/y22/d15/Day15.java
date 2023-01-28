84e16fe048baa0df36fc873f9b8c91b2af2b6307
Adam Jonas <adamjonas25@gmail.com>
2023-01-28 22:38:03 +0000

    Solve y22 d15 p1

--- y-2022/src/main/java/io/github/aj8gh/aoc/y22/d15/Day15.java
+++ y-2022/src/main/java/io/github/aj8gh/aoc/y22/d15/Day15.java
@@ -1,14 +1,95 @@
 package io.github.aj8gh.aoc.y22.d15;
 
-import java.util.List;
+import static java.lang.Math.abs;
+import static java.util.stream.IntStream.range;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Day15 {
 
-  public int part1(List<String> input) {
-    return 0;
+  private static final Pattern PATTERN = Pattern.compile("-?\\d+");
+  private static final long MULTIPLIER = 4_000_000;
+  private List<List<Integer>> digits;
+  private Set<List<Integer>> beacons;
+  private Set<List<Integer>> sensors;
+
+  public long part1(List<String> input, int rowToCheck) {
+    parse(input);
+    var maxDiff = Collections.max(digits.stream().map(this::getDiff).toList());
+    var xDigits = getBeacons(digits);
+    var maxX = Collections.max(xDigits);
+    var minX = Collections.min(xDigits);
+
+    return range(minX - maxDiff, maxX + maxDiff)
+        .filter(i -> isNotBeacon(i, rowToCheck) && isCovered(i, rowToCheck))
+        .count();
   }
 
-  public int part2(List<String> input) {
-    return 0;
+  public long part2(List<String> input, int max) {
+    parse(input);
+    for (int i = 0; i <= max; i++) {
+      for (int j = 0; j <= max; j++) {
+        if (isNotBeacon(i, j) && !isSensor(i, j) && !isCovered(i, j)) {
+          return i * MULTIPLIER + j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  private void parse(List<String> input) {
+    digits = input.stream()
+        .map(line -> PATTERN.matcher(line).results()
+            .map(MatchResult::group)
+            .map(Integer::parseInt).toList())
+        .toList();
+
+    beacons = extract(2, 4);
+    sensors = extract(0, 2);
+  }
+
+  private boolean isCovered(int x, int rowToCheck) {
+    for (var line : digits) {
+      var diffX = abs(line.get(0) - line.get(2));
+      var diffY = abs(line.get(1) - line.get(3));
+      var diff = diffX + diffY;
+
+      var newDiffY = abs(line.get(1) - rowToCheck);
+      var newDiffX = abs(line.get(0) - x);
+      if (newDiffX + newDiffY <= diff) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isNotBeacon(int x, int rowToCheck) {
+    return !beacons.contains(List.of(x, rowToCheck));
+  }
+
+  private boolean isSensor(int x, int rowToCheck) {
+    return sensors.contains(List.of(x, rowToCheck));
+  }
+
+  private List<Integer> getBeacons(List<List<Integer>> digits) {
+    return digits.stream()
+        .map(line -> List.of(line.get(0), line.get(2)))
+        .flatMap(List::stream)
+        .toList();
+  }
+
+  private int getDiff(List<Integer> line) {
+    return abs(line.get(0) - line.get(2));
+  }
+
+  private Set<List<Integer>> extract(int a, int b) {
+    return new HashSet<>(digits.stream().map(line -> line.subList(a, b)).toList());
   }
 }
--- y-2022/src/test/java/io/github/aj8gh/aoc/y22/d15/Day15Test.java
+++ y-2022/src/test/java/io/github/aj8gh/aoc/y22/d15/Day15Test.java
@@ -23,30 +23,36 @@ class Day15Test extends InputProvider {
 
   @ParameterizedTest
   @MethodSource(value = INPUT_PROVIDER_PART_1)
-  void part1(List<String> input, int expected) {
-    var actual = classUnderTest.part1(input);
+  void part1(List<String> input, int rowToCheck, int expected) {
+    var actual = classUnderTest.part1(input, rowToCheck);
     assertEquals(expected, actual);
   }
 
   @ParameterizedTest
   @MethodSource(value = INPUT_PROVIDER_PART_2)
-  void part2(List<String> input, int expected) {
-    var actual = classUnderTest.part2(input);
+  void part2(List<String> input, int upperBound, int expected) {
+    var actual = classUnderTest.part2(input, upperBound);
     assertEquals(expected, actual);
   }
 
   private static Stream<Arguments> inputProviderPart1() {
-    return getInput(26, 0);
+    return getInput(
+        26, 10,
+        6_078_701, 2_000_000
+    );
   }
 
   private static Stream<Arguments> inputProviderPart2() {
-    return getInput(0, 0);
+    return getInput(
+        56_000_011, 20,
+        0, 4_000_000
+    );
   }
 
-  private static Stream<Arguments> getInput(int example, int result) {
+  private static Stream<Arguments> getInput(int example, int i, int result, int j) {
     return Stream.of(
-        Arguments.of(reader().getExample(DAY_15).asStringList(), example),
-        Arguments.of(reader().getInput(DAY_15).asStringList(), result)
+        Arguments.of(reader().getExample(DAY_15).asStringList(), i, example),
+        Arguments.of(reader().getInput(DAY_15).asStringList(), j, result)
     );
   }
 }
