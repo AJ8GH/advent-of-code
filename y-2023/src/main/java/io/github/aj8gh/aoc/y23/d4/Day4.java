fdd6a0e39407f854cb8641a24d53ee7dd0f22bb3
Adam Jonas <adamjonas25@gmail.com>
2023-12-04 17:45:06 +0000

    Solve y23 d4 l1

--- y-2023/src/main/java/io/github/aj8gh/aoc/y23/d4/Day4.java
+++ y-2023/src/main/java/io/github/aj8gh/aoc/y23/d4/Day4.java
@@ -1,14 +1,44 @@
 package io.github.aj8gh.aoc.y23.d4;
 
-import java.util.List;
+import static java.lang.Math.pow;
 
+import java.util.List;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Day4 {
 
-  public int part1(List<String> input) {
-    return 0;
+  private static final Pattern CARD_PATTERN = Pattern.compile(".*\\d:\\s+(.*)\\s+\\|\\s+(.*)");
+  private static final Pattern DIGITS = Pattern.compile("\\d+");
+  private static final int FIRST = 1;
+  private static final int SECOND = 2;
+
+  public long part1(List<String> input) {
+    return input.stream()
+        .map(this::toScore)
+        .reduce(0L, Long::sum);
   }
 
-  public int part2(List<String> input) {
-    return 0;
+  public long part2(List<String> input) {
+    return 0L;
+  }
+
+  private long toScore(String card) {
+    var matcher = CARD_PATTERN.matcher(card);
+    matcher.find();
+    return (long) (pow(2, (toInts(matcher.group(FIRST))
+        .stream()
+        .filter(toInts(matcher.group(SECOND))::contains)
+        .count() - 1)));
+  }
+
+  private List<Integer> toInts(String s) {
+    return DIGITS.matcher(s)
+        .results()
+        .map(MatchResult::group)
+        .map(Integer::parseInt)
+        .toList();
   }
 }
--- y-2023/src/test/java/io/github/aj8gh/aoc/y23/d4/Day4Test.java
+++ y-2023/src/test/java/io/github/aj8gh/aoc/y23/d4/Day4Test.java
@@ -21,27 +21,27 @@ class Day4Test extends InputProvider {
 
   @ParameterizedTest
   @MethodSource(value = INPUT_PROVIDER_PART_1)
-  void part1(List<String> input, int expected) {
+  void part1(List<String> input, long expected) {
     var actual = classUnderTest.part1(input);
     assertEquals(expected, actual);
   }
 
   @ParameterizedTest
   @MethodSource(value = INPUT_PROVIDER_PART_2)
-  void part2(List<String> input, int expected) {
+  void part2(List<String> input, long expected) {
     var actual = classUnderTest.part2(input);
     assertEquals(expected, actual);
   }
 
   private static Stream<Arguments> inputProviderPart1() {
-    return getInput(8, 0);
+    return getInput(13L, 24175L);
   }
 
   private static Stream<Arguments> inputProviderPart2() {
-    return getInput(0, 0);
+    return getInput(30L, 0L);
   }
 
-  private static Stream<Arguments> getInput(int example, int result) {
+  private static Stream<Arguments> getInput(long example, long result) {
     return Stream.of(
         Arguments.of(reader().getExample(DAY_4).asStringList(), example),
         Arguments.of(reader().getInput(DAY_4).asStringList(), result)
